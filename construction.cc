dd113d7f5d7570b7740fd85c20c4465ea6278e13
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-10 14:55:02 +0200

    Update construction.cc

--- construction.cc
+++ construction.cc
@@ -27,6 +27,7 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct(){
 
 	G4double energy[2] = {1.239841939*eV/0.2,1.239841939*eV/0.2/0.9};
 	G4double rindexAerogel[2]={1.1,1.1};
+	G4double rindexWorld[2]={1.0,1.0};
 
 	G4MaterialPropertiesTable *mptAerogel = new G4MaterialPropertiesTable();
 	mptAerogel->AddProperty("RINDEX",energy,rindexAerogel,2);
