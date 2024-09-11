2c95dca04979c6f06385cf20367366a1e76b7d45
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-11 10:04:53 +0200

    Update detector.cc

--- detector.cc
+++ detector.cc
@@ -6,4 +6,13 @@ MySensitiveDetector::~MySensitiveDetector()
 {}
 
 G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist)
-{}
+{
+  G4Track *track = aStep->GetTrack();
+
+  G4StepPoint *preStepPoint = aStep->GetPreStepPoint();
+  G4StepPoint *postStepPoint = aStep->GetPostStepPoint();
+
+  G4ThreeVector posPhoton = preStepPoint->GetPossition();
+
+  G4cout << "Photon position: "<<posPosition << G4endl;
+}
