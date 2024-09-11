557344e3625f482efb61e8cda65bc7d103eb3259
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-11 12:28:09 +0200

    Update construction.cc

--- construction.cc
+++ construction.cc
@@ -40,7 +40,7 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct(){
 
 	worldMat->SetMaterialPropertiesTable(mptWorld);
 
-    	G4Box *solidWorld = new  G4Box("solidWorld",0.5*m,0.5*m,0.5*m);
+    	G4Box *solidWorld = new  G4Box("solidWorld",0.5*m,0.5*m,0.5*m); //mezzo lato del box
 
     	G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
     	
@@ -59,7 +59,7 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct(){
 	double n=10.;
 	for(G4int i = 0; i<n; i++){
 		for(G4int j=0;j<n;j++){
-			G4VPhysicalVolume *physDetector = new G4PVPlacement(0,G4ThreeVector(-0.5*m+(i+0.5)*m/n,-0.5*m+(j+0.5)*m/n,0.49*m),
+			G4VPhysicalVolume *physDetector = new G4PVPlacement(0,G4ThreeVector(-0.5*m+(i+0.5)*m/n,-0.5*m+(j+0.5)*m/n,0.49*m),//solido logico-> fisico
 			logicDetector,"physDetector",logicWorld,false,i*100+j,true);
 		}
 	}	
