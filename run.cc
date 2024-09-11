df000918f551009c3822b5262381e1845f7dd3ec
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-11 11:08:17 +0200

    Update run.cc

--- run.cc
+++ run.cc
@@ -7,7 +7,15 @@ MyRunAction::~MyRunAction()
 {}
 
 void MyRunAction::BeginOfRunAction(const G4Run*)
-{}
+{
+  G4AnalysisManager *man = G4AnalysisManager::Instance();
+  man->OpenFile("output.root");  
+}
 
 void MyRunAction::EndOfRunAction(const G4Run*)
-{}
+{
+  G4AnalysisManager *man = G4AnalysisManager::Instance();
+  man->Write();
+  man->CloseFile();  
+  
+}
