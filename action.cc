d5f70b586416d1d72ca8aa6bcf427494af5da46c
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-11 12:04:44 +0200

    Update action.cc

--- action.cc
+++ action.cc
@@ -7,9 +7,11 @@ MyActionInitialization::~MyActionInitialization()
 {}
 void MyActionInitialization::Build() const
 {
+    G4cout<<"MyActionInitialization::Build()"<<G4endl;
     MyPrimaryGenerator *generator = new MyPrimaryGenerator();
     SetUserAction(generator);
 
     MyRunAction *runAction = new MyRunAction();
     SetUserAction(runAction);
+    G4cout<<"MyActionInitialization::Build()::end"<<G4endl;
 }
