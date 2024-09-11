54dac88e87c46f4a4660defb11119a18b7c1fd93
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-11 09:43:11 +0200

    Update construction.cc

--- construction.cc
+++ construction.cc
@@ -66,3 +66,11 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct(){
 	
 	return physWorld;
 }
+
+void MyDetectorConstruction::ConstructSDandField()
+{
+	MySensitiveDetector *sensDet= new MySensitiveDetector("SensitiveDetector");
+	logicDetector->SetSensitiveDetector(sensDet);
+}
+
+
