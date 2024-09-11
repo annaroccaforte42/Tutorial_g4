f2db55148cf2d00fd7b161ed8b53c02b5512f02e
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-11 15:24:23 +0200

    Update physics.cc

--- physics.cc
+++ physics.cc
@@ -2,7 +2,7 @@
 
 MyPhysicsList::MyPhysicsList(){
 
-    RegisterPhysics(new G4EmStandardPhysics());
+    RegisterPhysics(new G4EmStandardPhysics_option4());
 //    RegisterPhysics(new G4OpticalPhysics());
 }
 MyPhysicsList::~MyPhysicsList(){}
