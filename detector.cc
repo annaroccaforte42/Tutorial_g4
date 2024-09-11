4194edcd61b0edec111205c84c60370a9f156aad
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-11 10:36:35 +0200

    Update detector.cc

--- detector.cc
+++ detector.cc
@@ -9,10 +9,22 @@ G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhis
 {
   G4Track *track = aStep->GetTrack();
 
+  track->SetTrackStatus(fStopAndKill);
+
   G4StepPoint *preStepPoint = aStep->GetPreStepPoint();
   G4StepPoint *postStepPoint = aStep->GetPostStepPoint();
 
   G4ThreeVector posPhoton = preStepPoint->GetPosition();
 
-  G4cout << "Photon position: "<<posPhoton << G4endl;
+  //G4cout << "Photon position: "<<posPhoton << G4endl;
+
+  const G4VTouchable *touchable = aStep->GetPreStepPoint()->GetTouchable;
+  G4int copyNo = touchable->GetCopyNumber();
+
+  G4cout<<"Copy number " << copyNo <<G4endl;
+
+  G4VPhysicalVolume *physVol = touchable->GetVolume();
+  G4ThreeVector posDetector = physVol->GetTranslation();
+
+  G4cout<<"Detector position: "<<posDetector<<G4endl;
 }
