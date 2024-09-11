70c6f63e7063b7b45695a4e0631a0fa47b6933ce
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-11 10:49:52 +0200

    Create run.hh

---
+++ run.hh
@@ -0,0 +1,14 @@
+#ifndef RUN_HH
+#define RUN_HH
+#INCLUDE "G4UserRunAction.hh"
+
+class MyRunAction : public G4UserRunAction
+{
+public:
+  MyRunAction();
+  ~MyRunAction();
+
+  virtual void BeginOfRunAction(const G4Run*);
+  virtual void EndofRunAction(const G4Run*);
+}
+#endif
