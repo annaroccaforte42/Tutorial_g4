c03d672ca16908abca180211cd85bebecdf9d67b
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-17 18:00:57 +0200

    Update construction.hh

--- construction.hh
+++ construction.hh
@@ -1,4 +1,3 @@
-// trick to not include multiple times
 #ifndef CONSTRUCTION_HH
 #define CONSTRUCTION_HH
 
@@ -6,7 +5,6 @@
 #include "G4VPhysicalVolume.hh"
 #include "G4LogicalVolume.hh"
 #include "G4Box.hh"
-// per le unità di misura tipo metri m
 #include "G4SystemOfUnits.hh"
 #include "G4Material.hh"
 #include "G4NistManager.hh"
@@ -17,16 +15,12 @@
 class MyDetectorConstruction : public G4VUserDetectorConstruction
 {
 public:
-	// need contructor and destructor
 	MyDetectorConstruction();
 	~MyDetectorConstruction();
 
 	virtual G4VPhysicalVolume *Construct();
-	// virtual because it is defined in G4USerDetectorConstruction
 
 private:
-	// the logicDetector defined in Construct method must be available also in other methods
-	// so we define it here
 	G4LogicalVolume *logicDetector;
 
 	virtual void ConstructSDandField();
