8dd838aa84317e70ed617f59fea83592014410c5
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-17 18:04:04 +0200

    Update physics.cc

--- physics.cc
+++ physics.cc
@@ -4,10 +4,6 @@
 
 MyPhysicsList::MyPhysicsList()
 {
-	// the aim here is to produce Cerenkov effect
-	// so we need two interactions
-	// he advises to use only the physics that is required for your application
-	// the more physics you implement the longer it takes for calculations
 	RegisterPhysics (new G4EmStandardPhysics());
 	RegisterPhysics (new G4OpticalPhysics());
 }
