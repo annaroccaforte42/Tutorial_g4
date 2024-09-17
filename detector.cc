c346f13de571e5fcc8103059231dc9fa1524e0db
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-17 16:51:47 +0200

    Update detector.cc

--- detector.cc
+++ detector.cc
@@ -2,13 +2,13 @@
 
 MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name)
 {
-  G4cout << "MySensitiveDetector::MySensitiveDetector" << G4endl;
+  //G4cout << "MySensitiveDetector::MySensitiveDetector" << G4endl;
 }
 MySensitiveDetector::~MySensitiveDetector()
 {}
 
 G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist)
 {
-  G4cout<<"MySensitiveDetector::ProcessHits"<<G4endl;
-  G4cout<<"MySensitiveDetector::ProcessHits::end"<<G4endl;
+  //G4cout<<"MySensitiveDetector::ProcessHits"<<G4endl;
+  //G4cout<<"MySensitiveDetector::ProcessHits::end"<<G4endl;
 }
