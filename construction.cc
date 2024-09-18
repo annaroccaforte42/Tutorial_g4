305d484e1cf904fde7d0dad7df67fb32b19e9013
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-18 14:50:16 +0200

    Update construction.cc

--- construction.cc
+++ construction.cc
@@ -14,25 +14,28 @@ MyDetectorConstruction::MyDetectorConstruction()
 MyDetectorConstruction::~MyDetectorConstruction()
 {}
 
-G4VPhysicalVolume* MyDetectorConstruction::Construct()
-{
+void MyDetectorConstruction::DefineMaterial(){
 	G4NistManager *nist = G4NistManager::Instance();
-
-	G4Material *SiO2 = new G4Material("SiO2", 2.201*g/cm3, 2);
+	SiO2 = new G4Material("SiO2", 2.201*g/cm3, 2);
 	SiO2->AddElement(nist->FindOrBuildElement("Si"), 1);
 	SiO2->AddElement(nist->FindOrBuildElement("O"), 2);
 
-	G4Material *H2O = new G4Material("H2O", 1.000*g/cm3, 2);
+	H2O = new G4Material("H2O", 1.000*g/cm3, 2);
 	H2O->AddElement(nist->FindOrBuildElement("H"), 2);
 	H2O->AddElement(nist->FindOrBuildElement("O"), 1);
+		
+	C = nist->FindOrBuildElement("C");
 
-	G4Element *C = nist->FindOrBuildElement("C");
-
-	G4Material *Aerogel = new G4Material("Aerogel", 0.200*g/cm3, 3);
+	Aerogel = new G4Material("Aerogel", 0.200*g/cm3, 3);
 	Aerogel->AddMaterial(SiO2, 62.5*perCent);
 	Aerogel->AddMaterial(H2O, 27.4*perCent);
 	Aerogel->AddElement(C, 0.1*perCent);
 
+	worldMat = nist->FindOrBuildMaterial("G4_AIR");
+}
+
+G4VPhysicalVolume* MyDetectorConstruction::Construct()
+{
 	G4double energy[2] = {1.2398*eV*um/0.2/um, 1.2398*eV*um/0.9/um};
 	G4double rindexAerogel[2] = {1.1, 1.1};
 	G4double rindexWorld[2] = {1.0, 1.0};
@@ -42,7 +45,6 @@ G4VPhysicalVolume* MyDetectorConstruction::Construct()
 
 	Aerogel->SetMaterialPropertiesTable(mptAerogel);
 
-	G4Material *worldMat = nist->FindOrBuildMaterial("G4_AIR");
 
 	G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable();
 	mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2);
