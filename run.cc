7c2f13210dd29076e5fff0012bb07bf7784a1555
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-18 10:54:35 +0200

    Update run.cc

--- run.cc
+++ run.cc
@@ -17,7 +17,7 @@ MyRunAction::~MyRunAction()
 void MyRunAction::BeginOfRunAction(const G4Run*)
 {
   G4AnalysisManager *man = G4AnalysisManager::Instance();
-  G4int runNumber = run->GetRunID();
+  G4int runID = run->GetRunID();
   std::stringstream strRunID;
   strRunID<<runID;
   man->OpenFile("output.root"+strRunID.str()+".root");
