96bd298baabf9f5b4bdddd165481553d22b44e78
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-18 15:27:56 +0200

    Update construction.cc

--- construction.cc
+++ construction.cc
@@ -9,7 +9,7 @@ MyDetectorConstruction::MyDetectorConstruction()
 
     fMessenger->DeclareProperty("nCols", nCols, "Number of cols");
     fMessenger->DeclareProperty("nRows", nRows, "Number of rows");
-    DefineMaterials();
+    DefineMaterial();
 }
 
 MyDetectorConstruction::~MyDetectorConstruction()
