b2916e0c19cdfcbba42ca7eb440b20c35fa00b30
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-18 10:52:03 +0200

    Update run.cc

--- run.cc
+++ run.cc
@@ -2,6 +2,13 @@
 
 MyRunAction::MyRunAction()
 {
+  G4AnalysisManager *man = G4AnalysisManager::Instance();
+  man->CreateNtuple("Hits", "Hits");
+  man->CreateNtupleIColumn("fEvent"); // event number
+  man->CreateNtupleDColumn("fX"); // x coordinate of the detector
+  man->CreateNtupleDColumn("fY"); // y
+  man->CreateNtupleDColumn("fZ"); // z
+  man->FinishNtuple(0);
 }
 
 MyRunAction::~MyRunAction()
@@ -10,15 +17,12 @@ MyRunAction::~MyRunAction()
 void MyRunAction::BeginOfRunAction(const G4Run*)
 {
   G4AnalysisManager *man = G4AnalysisManager::Instance();
+  G4int runNumber = run->GetRunID();
+  std::stringstream strRunID;
+  strRunID<<runID;
+  man->OpenFile("output.root"+strRunID.str()+".root");
 
-  man->OpenFile("output.root");
 
-  man->CreateNtuple("Hits", "Hits");
-  man->CreateNtupleIColumn("fEvent"); // event number
-  man->CreateNtupleDColumn("fX"); // x coordinate of the detector
-  man->CreateNtupleDColumn("fY"); // y
-  man->CreateNtupleDColumn("fZ"); // z
-  man->FinishNtuple(0);
 }
 
 void MyRunAction::EndOfRunAction(const G4Run*)
