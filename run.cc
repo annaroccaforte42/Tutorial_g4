70637526cd684df826d87bc15aaf77ac191695db
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-18 11:27:34 +0200

    Update run.cc

--- run.cc
+++ run.cc
@@ -20,7 +20,7 @@ void MyRunAction::BeginOfRunAction(const G4Run* run)
   G4int runID = run->GetRunID();
   std::stringstream strRunID;
   strRunID<<runID;
-  man->OpenFile("output.root"+strRunID.str()+".root");
+  man->OpenFile("output"+strRunID.str()+".root");
 
 
 }
