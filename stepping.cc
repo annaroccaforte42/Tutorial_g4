a695046a325f9198f602add3a8871b72655dbf17
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-20 10:46:53 +0200

    Update stepping.cc

--- stepping.cc
+++ stepping.cc
@@ -7,11 +7,9 @@ MySteppingAction::~MySteppingAction(){
 }
 void MySteppingAction::UserSteppingAction(const G4Step *step)
 {
-  G4LogicalVolume *volume = step->GetPreStepPoint()->GetTouchableHandle()-
->GetVolume()->GetLogicalVolume();
+  G4LogicalVolume *volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
 
-  const MyDetectorConstruction() *detectorConstruction=static_cast<const
-MyDetectorConstruction*> (G4RunManager::GetRunManager() ->GetScoringVolume();
+  const MyDetectorConstruction() *detectorConstruction=static_cast<const MyDetectorConstruction*> (G4RunManager::GetRunManager() ->GetScoringVolume();
 
   if(volume != fScoringVolume)
     return;
