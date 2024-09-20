97cc36ad938d31d74f0b76a2f96483a5decdb391
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-20 15:47:19 +0200

    Update construction.hh

--- construction.hh
+++ construction.hh
@@ -30,8 +30,7 @@ private:
 	virtual void ConstructSDandField();
 	G4int nCols, nRows;
 	G4double xWorld,yWorld,zWorld;
-	G4Box *solidWorld, *solidRadiator, *solidDetector;
-	G4Tube *solidScintillator;
+	G4Box *solidWorld, *solidRadiator, *solidDetector, *solidScintillator;
 	
 	G4LogicalVolume *logicWorld, *logicRadiator, *logicScintillator;
 	G4VPhysicalVolume *physWorld, *physRadiator, *physDetector, *physScintillator;
