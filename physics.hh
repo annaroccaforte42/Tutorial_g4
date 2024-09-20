8d0197c045c5e0d5946a4f4c4385dddf86e206d7
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-20 10:44:59 +0200

    Update physics.hh

--- physics.hh
+++ physics.hh
@@ -2,6 +2,10 @@
 #define PHYSICS_HH
 
 #include "G4VModularPhysicsList.hh"
+#include "G4EmStandardPhysics.hh"
+#include "G4OpticalPhysics.hh"
+#include "G4DecayPhysics.hh"
+#include "G4RadioactiveDecayPhysics.hh"
 
 class MyPhysicsList : public G4VModularPhysicsList
 {
