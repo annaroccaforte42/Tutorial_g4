55c401893c315c1cdbf02e8e966d201403679b3c
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-20 09:53:43 +0200

    Update event.cc

--- event.cc
+++ event.cc
@@ -12,7 +12,7 @@ void MyEventAction::BeginOfEventAction(const G4Event*){
 void MyEventAction::EndOfEventAction(const G4Event*){
   G4cout << "Energy deposition: "<<fEdep<<G4endl;
 
-  G4Analysis *man = G4AnalysisManager::Instance();
+  G4AnalysisManager *man = G4AnalysisManager::Instance();
   
   man->FillNtupleDColumn(2,0,fEdep);
   
