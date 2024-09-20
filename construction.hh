43b459615a3246d8253d55758919d251fbdb0159
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-20 11:43:23 +0200

    Update construction.hh

--- construction.hh
+++ construction.hh
@@ -5,7 +5,7 @@
 #include "G4VPhysicalVolume.hh"
 #include "G4LogicalVolume.hh"
 #include "G4Box.hh"
-#include "G4Tabs.hh"
+#include "G4Tubs.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4Material.hh"
 #include "G4NistManager.hh"
