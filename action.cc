9bc6b31bde41d83e1c8c12f53e0cf4ccefa309a3
annaroccaforte42 <anna.roccaforte01@gmail.com>
2024-09-20 10:15:41 +0200

    Update action.cc

--- action.cc
+++ action.cc
@@ -17,4 +17,11 @@ void MyActionInitialization::Build() const
 
 	MyRunAction *runAction = new MyRunAction();
 	SetUserAction(runAction);
+
+	MyEventAction *eventAction = new MyEventAction(runAction);
+	SetUserAction(eventAction);
+
+	MySteppingAction *steppingAction = new MySteppingAction(eventAction);
+	SetUserAction(steppingAction);
+	
 }
